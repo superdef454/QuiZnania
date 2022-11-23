0c85b3195bb3e3773afcfc3c5597cb7b82154175
superdef454 <superdef454@mail.ru>
2022-11-23 16:00:49 +0300

    new file:   featuers/requirements.txt 	modified:   featuers/steps/test.py 	modified:   featuers/test.feature

--- featuers/steps/test.py
+++ featuers/steps/test.py
@@ -1,32 +1,45 @@
 from behave import *
+import os
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
+# path = os получить путь до драйвера для разных компов но лень, поэтому:
+path = "C:/Users/Пользователь/Desktop/работа/git/QuiZnania/featuers/requirements.txt"
+
 #Откроем главную страницу. Передадим в качестве аргумента адрес страницы.
-@given('website "{url}"')
+@step('Запущен сайт "{url}"')
 def step(context, url):
     chrome_options = Options()
     chrome_options.add_argument("--disable-extensions")
-    context.browser = webdriver.Chrome(executable_path="C:/Users/user/Documents/учёба/Работы/QuiZ/featuers/chromedriver.exe", chrome_options=chrome_options)
+    context.browser = webdriver.Chrome(executable_path=path, chrome_options=chrome_options)
     context.browser.maximize_window()
     context.browser.get(url)
 
-# найдём и нажмём на кнопку
-@When("push button with div '{text}'")
-def step(context, text):
-    element = WebDriverWait(context.browser, 120).until(
-        EC.element_to_be_clickable((By.CLASS_NAME, text))
-    )
-    element.click()
+@step("Пользователь выбирает первый раздел, первую тему, первый раунд и прокликивает до конца")
+def step(context):
+    pass
 
-#Проверим, что мы на странице с результатами поиска, есть некоторый искомый див
-@then("page include div '{text}'")
-def step(context, text):
-    element = WebDriverWait(context.browser, 120).until(
-        EC.presence_of_element_located((By.CLASS_NAME, text))
-    )
-    # assert element
+@step("Пользователь выбирает первый раздел, первую тему, первый раунд и прокликивает до конца")
+def step(context):
     context.browser.quit()
+
+# # найдём и нажмём на кнопку
+# @When("push button with div '{text}'")
+# def step(context, text):
+#     element = WebDriverWait(context.browser, 120).until(
+#         EC.element_to_be_clickable((By.CLASS_NAME, text))
+#     )
+#     element.click()
+
+# #Проверим, что мы на странице с результатами поиска, есть некоторый искомый див
+# @then("page include div '{text}'")
+# def step(context, text):
+#     element = WebDriverWait(context.browser, 120).until(
+#         EC.presence_of_element_located((By.CLASS_NAME, text))
+#     )
+#     # assert element
+#     context.browser.quit()
--- featuers/test.feature
+++ featuers/test.feature
@@ -1,5 +1,14 @@
-Feature: Check working
-Scenario: Сheck work site
-  Given website "http://127.0.0.1:8000/"
-  When push button with div 'fs_start_game'
-  Then page include div 'rounds-container'
+# Пример тестирования на английском языке
+# Feature: Check working
+# Scenario: Сheck work site
+#   Given website "http://127.0.0.1:8000/"
+#   When push button with div 'fs_start_game'
+#   Then page include div 'rounds-container'
+
+# language: ru
+
+Функционал: Работает ли основной функционал программы
+Сценарий: Прохождение полного цикла игры
+Дано Запущен сайт "http://localhost:8000/"
+Когда Пользователь выбирает первый раздел, первую тему, первый раунд и прокликивает до конца
+То воспроизводится фейерверк
