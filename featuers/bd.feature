627ade4ca7bb04e6d072ede7bc5347ac943a0b46
superdef454 <superdef454@mail.ru>
2022-11-28 14:19:09 +0300

    тестирование бд

--- featuers/bd.feature
+++ featuers/bd.feature
@@ -1,6 +1,7 @@
 # language: ru
 
 Функционал: Работает ли флаг отображения в БД
+Сценарий: Нажатие на флаг в бд
 Дано Страница "http://localhost:8000/sigame/" с объектом "/html/body/div/div/div/a"
-Когда На странице бд выключить отображение объекта "/html/body/div/div[3]/div/div[1]/div/div/div/form/div[4]/table/tbody/tr/td[2]/input" и сохранить изменения "/html/body/div/div[3]/div/div[1]/div/div/div/form/p/input"
+Когда На странице бд "http://localhost:8000/admin/sigame/section/" выключить отображение объекта "/html/body/div/div[3]/div/div[1]/div/div/div/form/div[4]/table/tbody/tr/td[2]/input" и сохранить изменения "/html/body/div/div[3]/div/div[1]/div/div/div/form/p/input"
 То На странице "http://localhost:8000/sigame/" пропадёт объект "/html/body/div/div/div/a"
---
+++ featuers/steps/step.py
@@ -0,0 +1,77 @@
+from behave import *
+import os
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+# path = os получить путь до драйвера для разных компов но лень, поэтому:
+path = "C:/Users/Пользователь/Desktop/работа/git/QuiZnania/featuers/chromedriver.exe"
+
+#Откроем главную страницу. Передадим в качестве аргумента адрес страницы.
+@given('Запущен сайт "{url}"')
+def step(context, url):
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-extensions")
+    context.browser = webdriver.Chrome(executable_path=path, chrome_options=chrome_options)
+    context.browser.maximize_window()
+    context.browser.get(url)
+
+@when('Пользователь нажимает на кнопку с названием "{name}"')
+def step(context, name):
+    element = WebDriverWait(context.browser, 120).until(
+        EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[1]/a"))
+    )
+    element.click()
+
+@then("Открывается страница с выбром раздела")
+def step(context):
+    element = WebDriverWait(context.browser, 120).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'text__third__screen'))
+    )
+    assert element
+    context.browser.quit()
+
+# bd.py
+
+@given('Страница "{url}" с объектом "{object}"')
+def step(context, url, object):
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-extensions")
+    context.browser = webdriver.Chrome(executable_path=path, chrome_options=chrome_options)
+    context.browser.maximize_window()
+    context.browser.get(url)
+    try:
+        context.check = WebDriverWait(context.browser, 2).until(
+            EC.presence_of_element_located((By.XPATH, object))
+        )
+    except Exception as e:
+        context.check = False
+
+@when('На странице бд "{url}" выключить отображение объекта "{checkbox}" и сохранить изменения "{submit}"')
+def step(context, url, checkbox, submit):
+    context.browser.get(url)
+    element = WebDriverWait(context.browser, 10).until(
+        # Как найти по тексту хз
+        EC.element_to_be_clickable((By.XPATH, checkbox))
+    ).click()
+    element = WebDriverWait(context.browser, 10).until(
+        # Как найти по тексту хз
+        EC.element_to_be_clickable((By.XPATH, submit))
+    ).click()
+
+    
+@then('На странице "{url}" пропадёт объект "{object}"')
+def step(context, url, object):
+    context.browser.get(url)
+    try:
+        element = WebDriverWait(context.browser, 2).until(
+            EC.presence_of_element_located((By.XPATH, object))
+        )
+    except Exception as e:
+        element = False
+    context.browser.quit()
+    assert element != context.check, "Объект не изменился"
+    
